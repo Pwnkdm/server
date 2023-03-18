42b68e7cc554c972d7b496e0d77c35cb7fe5d086
Pwnkdm <pawanrajkadam@gmail.com>
2023-03-18 19:43:02 +0530

    .

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 exports.connectDatabase = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect("mongodb://localhost:27017/insta")
     .then((con) => console.log(`Database connected:${con.connection.host}`))
     .catch((error) => console.log(error));
 };
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const app = require("./app");
 const { connectDatabase } = require("./config/database");
 
-connectDatabase();
-
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
+  await connectDatabase();
   console.log(`Server is running on port ${process.env.PORT}`);
 });
